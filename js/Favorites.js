1559ae1c642e0a9d0d564497469e9a698e4d2816
Sérgio Gomes <sergiotecdev@gmail.com>
2022-10-14 15:30:12 -0300

    Correção de erro

--- js/Favorites.js
+++ js/Favorites.js
@@ -1,100 +1,88 @@
 // classe que vai conter a lógica dos dados
 // como os dados serão estruturados
-
 export class Favorites {
-    constructor(root) {
-        this.root = document.querySelector(root)
-        this.load()
-    }
-
-    update() {
-        this.removeAllTr()
-      
-
-        entries.forEach(user => {
-          const row = this.creatRow()
-        })
-         
-      }
-
-      load() {
-        this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
-      }
-
-      delete(user) {
-        const filteredEntries = this.entries.filter(entry => 
-        entry.login !== user.login)
-
-      this.entries = filteredEntries
-      this.update()
-      }
+  constructor(root) {
+    this.root = document.querySelector(root)
+    this.load()
   }
+
+  load() {
+    this.entries = JSON.parse(localStorage.getItem('@github-favorites:')) || []
+  }
+
+  delete(user) {
+    const filteredEntries = this.entries
+      .filter(entry => entry.login !== user.login)
+
+    this.entries = filteredEntries
+    this.update()
+  }
+}
 
 // classe que vai criar a visualização e eventos do HTML
 export class FavoritesView extends Favorites {
-    constructor(root) {
-        super(root)
+  constructor(root) {
+    super(root)
 
-        this.tbody = this.root.querySelector('table tbody')
+    this.tbody = this.root.querySelector('table tbody')
 
-        this.update()
-    }
+    this.update()
+  }
 
-    update() {
-      this.removeAllTr()
+  update() {
+    this.removeAllTr()
 
+    this.entries.forEach( user => {
+      const row = this.createRow()
 
-      this.entries.forEach( user => {
-        const row = this.createRow()
-        
-        row.querySelector('.user img').src = `https://github.com/${user.login}.png`
-        row.querySelector('.user img').alt = `Imagem de ${user.name}`
-        row.querySelector('.user p').textContent = user.name
-        row.querySelector('.user span').textContent = user.login
-        row.querySelector('.repositories').textContent = user.public_repos
-        row.querySelector('.followers').textContent = user.followers
+      row.querySelector('.user img').src = `https://github.com/${user.login}.png`
+      row.querySelector('.user img').alt = `Imagem de ${user.name}`
+      row.querySelector('.user p').textContent = user.name
+      row.querySelector('.user span').textContent = user.login
+      row.querySelector('.repositories').textContent = user.public_repos
+      row.querySelector('.followers').textContent = user.followers
 
-        row.querySelector('.remove').onclick = () => {
-          const isOK = confirm ('Tem certeza que deseja deletar essa linha?')
-          if(isOK) {
-            this.delete(user)
-          }
-          
-         
+      row.querySelector('.remove').onclick = () => {
+        const isOk = confirm('Tem certeza que deseja deletar essa linha?')
+        if(isOk) {
+          this.delete(user)
         }
-        this.tbody.append(row)
-      })
-    }
+      }
 
-  creatRow() {
+      this.tbody.append(row)
+    })
+  }
+
+  createRow() {
     const tr = document.createElement('tr')
+
     tr.innerHTML = `
       <td class="user">
-          <img src="https://github.com/diego3g.png" alt="">
-            <a href="https://github.com/diego3g" target="_blank">
-                <p>Diego Fernandes</p>
-                <span>diego3g</span>
-            </a>
-        </td>
-        <td class="repositories">
-            48
-        </td>
-        <td class="followers">
-            22503
-        </td>
-        <td>
-            <button class="remove">&times;</button>
-        </td>
+        <img src="https://github.com/maykbrito.png" alt="Imagem de maykbrito">
+        <a href="https://github.com/maykbrito" target="_blank">
+          <p>Mayk Brito</p>
+          <span>maykbrito</span>
+        </a>
+      </td>
+      <td class="repositories">
+        76
+      </td>
+      <td class="followers">
+        9589
+      </td>
+      <td>
+        <button class="remove">&times;</button>
+      </td>
     `
+
     return tr
   }
 
   removeAllTr() {
     this.tbody.querySelectorAll('tr')
-        .forEach((tr) => {
-            tr.remove()
-        }) 
+      .forEach((tr) => {
+        tr.remove()
+      })  
   }
 }
-
 
